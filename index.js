6bdcb6c284089861a2eb3106d3c8ad0e1ac2173b
Miika Hänninen <miika.hanninen@reaktor.com>
2021-01-06 13:38:03 +0200

    Add graceful shutdown

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ const port = 8080
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM, stopping.')
+  server.close(() => {
+    console.log('Server stopped')
+  })
+})
